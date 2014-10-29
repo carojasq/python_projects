1e99914bbe1f7b4777abc724708bc85cde41a1e0
alejo0317 <alejandro.0317@gmail.com>
2014-10-28 23:42:39 -0500

    Added interpeter

--- Rosalind_bioinformatics_solutions/Countingdnanucleotides.py
+++ Rosalind_bioinformatics_solutions/Countingdnanucleotides.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python2
+
 from collections import OrderedDict
 #Solution for 	http://rosalind.info/problems/dna/
 
 in_seq = raw_input()
 ns = {'A': 0, 'C': 0, 'G': 0, 'T': 0} #nucleotides
 for n in in_seq: ns[n]+=1
+ns
 print "%i %i %i %i" % (ns['A'], ns['C'], ns['G'], ns['T'])
 
